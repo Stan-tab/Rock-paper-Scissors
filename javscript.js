bbb2278e913bc00d426a5d5b9310743c9a3088ff
Stan-tab <godofwarmafia2@gmail.com>
2024-11-17 09:25:08 +0500

    Fix bugs

--- javscript.js
+++ javscript.js
@@ -1,9 +1,16 @@
-let UserInput = parseInt(prompt("Input Rock-1; Paper-2; Scissors-3:"));
+let UserInput = parseInt(prompt("Round 1\nInput Rock-1; Paper-2; Scissors-3:"));
 let MachineChoise = Math.floor(Math.random() * 3);
+let humanScore = 0;
+let MachineScore = 0;
+round = 1;
 
 function start () {
-    UserInput = parseInt(prompt("Input Rock-1; Paper-2; Scissors-3:"));
+    if (round == 5){
+        return alert(`Gave finished\n Your score:${humanScore}\n Computer's score:${MachineScore}`)
+    }
+    UserInput = parseInt(prompt(`Round ${round}\nInput Rock-1; Paper-2; Scissors-3:`));
     MachineChoise = Math.floor(Math.random() * 3);
+    round += 1;
     return check(UserInput, MachineChoise)
 }
 
@@ -14,30 +21,37 @@ function check(UserInput, MachineChoise) {
     }
     if (UserInput == 1 && MachineChoise == 1) {
         alert("Machine won")
+        MachineScore += 1;
         return start()
     }
     if (UserInput == 1 && MachineChoise == 2) {
         alert("You won")
+        humanScore += 1
         return start()
     }
     if (UserInput == 2 && MachineChoise == 0) {
         alert("You won")
+        humanScore += 1
         return start()
     }
     if (UserInput == 2 && MachineChoise == 2) {
         alert("Machine won")
+        MachineScore += 1;
         return start()
     }
     if (UserInput == 3 && MachineChoise == 0) {
         alert("Machine won")
+        MachineScore += 1;
         return start()
     }
     if (UserInput == 3 && MachineChoise == 1) {
         alert("You won")
+        humanScore += 1
         return start()
     }
     alert("Not available value\nPlease write from 1 to 3")
-    // return start()
+    r -= 1
+    return start()
 }
 
 check(UserInput, MachineChoise)
