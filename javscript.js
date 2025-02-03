31e964846fd9e605b79a631f99e851fe34e4f309
Stan <75632684+Stan-tab@users.noreply.github.com>
2025-02-02 23:45:02 -0800

    Add files via upload

--- javscript.js
+++ javscript.js
@@ -1,81 +1,90 @@
-const div = document.querySelector(".mainbox");
-div.style.cssText = "margin: 0 auto; width: 30%; text-align: center;";
-const buttons = document.querySelectorAll(".mainbox button");
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-let p = document.createElement("p");
-let para = document.createElement("p");
-let para1 = document.createElement("p");
-p.style.marginBottom = '5px';
-para1.style.marginTop = '0';
-para.style.cssText = 'margin: 0 auto;';
-div.insertBefore(para1, div.firstChild);
-div.insertBefore(para, div.firstChild);
-div.insertBefore(p,  div.firstChild);
-let userInput;
-let round = 1;
-let MachineChoise;
-let humanScore = 0;
-let MachineScore = 0;
-
-buttons.forEach(btn => {
-    btn.style.cssText = "width:70px; height:30px";
-});
-
-rock.addEventListener("click", () => {
-    UserInput = 1;
-    logic();
-})
-paper.addEventListener("click", () => {
-    UserInput = 2;
-    logic();
-})
-scissors.addEventListener("click", () => {
-    UserInput = 3;
-    logic();
-})
+btns = {
+    rock : document.querySelector(".rock"),
+    paper : document.querySelector(".paper"),
+    scissors : document.querySelector(".scissors"),
+}
 
 // Begining
-function logic() {
-    MachineChoise = Math.floor(Math.random() * 3);
-    if (round == 6){
-        return [p.textContent = `Game finished!!!`,
-                para.textContent = `Your score:${humanScore}`,
-                para1.textContent = `Computer's score:${MachineScore}`];
+class gameLogic {
+    div = document.querySelector(".mainbox");
+    buttons = document.querySelectorAll(".mainbox button");
+    p = document.createElement("p");
+    para = document.createElement("p");
+    para1 = document.createElement("p");
+    userInput;
+    round = 1;
+    humanScore = 0;
+    MachineScore = 0;
+
+    constructor() {
+        this.div.style.cssText = "margin: 0 auto; width: 30%; text-align: center;";
+        this.p.style.marginBottom = '5px';
+        this.para1.style.marginTop = '0';
+        this.para.style.cssText = 'margin: 0 auto;';
+        this.div.insertBefore(this.para1, this.div.firstChild);
+        this.div.insertBefore(this.para, this.div.firstChild);
+        this.div.insertBefore(this.p,  this.div.firstChild);
+        this.buttons.forEach(btn => {
+            btn.style.cssText = "width:70px; height:30px";
+        });
+        this.userInput = this.userInput
     }
-    if (UserInput-1 == MachineChoise) {
-        p.textContent = "Draw";
-        round += 1;
-    }
-    if (UserInput == 1 && MachineChoise == 1) {
-        p.textContent = "Computer won";
-        round += 1;
-        MachineScore += 1;
-    }
-    if (UserInput == 1 && MachineChoise == 2) {
-        p.textContent = "You won";
-        humanScore += 1
-        round += 1;
-    }
-    if (UserInput == 2 && MachineChoise == 0) {
-        p.textContent = "You won";
-        humanScore += 1
-        round += 1;
-    }
-    if (UserInput == 2 && MachineChoise == 2) {
-        p.textContent = "Computer won";
-        MachineScore += 1;
-        round += 1;
-    }
-    if (UserInput == 3 && MachineChoise == 0) {
-        p.textContent = "Computer won";
-        MachineScore += 1;
-        round += 1;
-    }
-    if (UserInput == 3 && MachineChoise == 1) {
-        p.textContent = "You won";
-        humanScore += 1
-        round += 1;
+
+    logic = () => {
+        this.MachineChoise = Math.floor(Math.random() * 3);
+        if (this.round == 6){
+            return [this.p.textContent = `Game finished!!!`,
+                    this.para.textContent = `Your score:${this.humanScore}`,
+                    this.para1.textContent = `Computer's score:${this.MachineScore}`];
+        }
+        if (this.UserInput-1 == this.MachineChoise) {
+            this.p.textContent = "Draw";
+            this.round += 1;
+        }
+        if (this.UserInput == 1 && this.MachineChoise == 1) {
+            this.p.textContent = "Computer won";
+            this.round += 1;
+            this.MachineScore += 1;
+        }
+        if (this.UserInput == 1 && this.MachineChoise == 2) {
+            this.p.textContent = "You won";
+            this.humanScore += 1
+            this.round += 1;
+        }
+        if (this.UserInput == 2 && this.MachineChoise == 0) {
+            this.p.textContent = "You won";
+            this.humanScore += 1
+            this.round += 1;
+        }
+        if (this.UserInput == 2 && this.MachineChoise == 2) {
+            this.p.textContent = "Computer won";
+            this.MachineScore += 1;
+            this.round += 1;
+        }
+        if (this.UserInput == 3 && this.MachineChoise == 0) {
+            this.p.textContent = "Computer won";
+            this.MachineScore += 1;
+            this.round += 1;
+        }
+        if (this.UserInput == 3 && this.MachineChoise == 1) {
+            this.p.textContent = "You won";
+            this.humanScore += 1
+            this.round += 1;
+        }
     }
 }
+
+const gameStart = new gameLogic();
+
+btns.rock.addEventListener("click", () => {
+    gameStart.UserInput = 1;
+    gameStart.logic();
+})
+btns.paper.addEventListener("click", () => {
+    gameStart.UserInput = 2;
+    gameStart.logic();
+})
+btns.scissors.addEventListener("click", () => {
+    gameStart.UserInput = 3;
+    gameStart.logic();
+})
